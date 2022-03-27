9f9e03985699f35dfc4b9c31d7f0500542e04989
mouni186 <mounikas1861999@gmail.com>
2022-03-27 20:04:39 +0530

    feat: Butterfly api route created

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.get('/api-butterfly', (req, res) => {
+    const responseObject = {
+        message: "success",
+        status: 200
+    }
+    res.send(responseObject)
+});
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
